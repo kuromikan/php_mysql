2b0a455242bb0b43960bede801502cd1cb0b4bd1
YAO,WEI <gamania54088@gmail.com>
2016-02-23 14:51:18 +0800

    新增score筆數
    做 where 條件篩選用

--- sample_db.sql
+++ sample_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2016-02-23: 07:04:41
+-- 產生時間： 2016-02-23: 07:50:31
 -- 伺服器版本: 5.6.17
 -- PHP 版本： 5.5.12
 
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `score` (
   `subject` varchar(20) NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- 資料表的匯出資料 `score`
@@ -63,7 +63,9 @@ CREATE TABLE IF NOT EXISTS `score` (
 
 INSERT INTO `score` (`usr_id`, `score`, `subject`, `id`) VALUES
 ('S201600001', 60, 'C', 1),
-('S201600002', 65, 'C', 2);
+('S201600002', 65, 'C', 2),
+('S201600001', 75, 'database', 3),
+('S201600002', 85, 'database', 4);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
